a54495a9eab07d39dfb1123f63e5a85272295faf
ELWAER-M <em200508@gmail.com>
2022-06-02 21:00:41 +0200

    remove memory usage for now

--- bot/commands/stat.py
+++ bot/commands/stat.py
@@ -16,8 +16,8 @@ async def info_slash(ctx: tanjun.abc.SlashContext) -> None:
     await info(ctx)
 
 async def info(ctx: tanjun.abc.Context, /) -> None:
-    total_mem = int("".join(list(filter(lambda x: "MemTotal:" in x, open("/proc/meminfo", "r").read().splitlines()))).replace("MemTotal:        ", "").replace(" kB", ""))
-    av_mem = int("".join(list(filter(lambda x: "MemAvailable:" in x, open("/proc/meminfo", "r").read().splitlines()))).replace("MemAvailable:     ", "").replace(" kB", ""))
+    # total_mem = int("".join(list(filter(lambda x: "MemTotal:" in x, open("/proc/meminfo", "r").read().splitlines()))).replace("MemTotal:        ", "").replace(" kB", ""))
+    # av_mem = int("".join(list(filter(lambda x: "MemAvailable:" in x, open("/proc/meminfo", "r").read().splitlines()))).replace("MemAvailable:     ", "").replace(" kB", ""))
     system = platform.system()
     arch = platform.machine()
     py_ver = platform.python_version()
@@ -25,7 +25,7 @@ async def info(ctx: tanjun.abc.Context, /) -> None:
     tanjun_ver = tanjun.__version__
 
     await ctx.respond(embed=hikari.Embed(
-        description=f"**Used Memory:** `{(total_mem-av_mem)/1024/1024:.2f}Gb / {total_mem/1024/1024:.2F}Gb ({100.0*(total_mem-av_mem)/total_mem:.0f}%)`\n**System:** `{system}`\n**Architecture:** `{arch}`\n**Python**: `{py_ver}`\n**Hikari:** `{hikari_ver}`\n**Tanjun:** `{tanjun_ver}`",
+        description=f"**System:** `{system}`\n**Architecture:** `{arch}`\n**Python**: `{py_ver}`\n**Hikari:** `{hikari_ver}`\n**Tanjun:** `{tanjun_ver}`",
                         color="#ffe500"))
 
 load_command = component.make_loader()
